e5ee8c5fb173c2b11d40632310260dd3574f73b0
Jay B. Martin <jmartin805@gmail.com>
2013-03-16 21:21:25 -0400

    Added blicket detector functionality and materials for the 7 trial types found in Gopnik & Sobel (2000).

---
+++ assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h:
---
+++ main.d
@@ -0,0 +1,115 @@
+main.o: main.cpp \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/audio.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/cube.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/event.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/filesystem.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/metadata.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/motion.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/string.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/system.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/time.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/color.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/sprite.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/framebuffer.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0rom.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg1.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg2.h \
+  /Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/audio.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/cube.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/event.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/filesystem.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/metadata.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/motion.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/string.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/system.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/time.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/color.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/sprite.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/framebuffer.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0rom.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg1.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg2.h:
+
+/Users/j/Desktop/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/tilebuffer.h:
+
+assets.gen.h:
